c7fc4e69132d245331f7d153207f94eb34b70c91
Walid Amar <walidamarpro@gmail.com>
2022-05-28 16:07:32 -0400

    themes set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
 import { Layout } from './components/Layout';
+import {ThemeContext, themes} from './context/themeContext';
+import React, {useState} from 'react';
+import ToggleSwitch from './components/ToggleSwitch';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const [theme, setTheme] = useState(themes);
+
+  const toggleTheme = () => {
+    setTheme((previousThemeValue) => 
+      previousThemeValue === themes.dark ? themes.light : themes.dark
+    );
+  };
+
+  const toggleThemeColor = () => {
+    setTheme((previousThemeValue) => 
+      previousThemeValue === themes.orange ? themes.purple : themes.orange
+    );
+  };
+
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <ThemeContext.Provider value={{ theme: theme }}>  
+      <div>
+        <div className='toggle'>
+          <div>
+            <p className='colorText'>Light/Dark</p>
+            <ToggleSwitch onToggle={toggleTheme}/>
+          </div>
+          <div>
+            <p className='colorText'>Purple/Orange</p>
+            <ToggleSwitch onToggle={toggleThemeColor}/>
+          </div>
+          
+          
+        </div>
+        <div className='navbar'>
+          <Navbar/>
+        </div>
+        
+        <div className="App" style={{ backgroundColor: theme.background, color: theme.foreground }}>
+          <Layout />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useFetchProducts from '../../hooks/products/useFetchProducts';
 import Spinner from '../Spinner';
 import { ProductListRow } from './ProductListRow';
+import { ThemeContext } from '../../context/themeContext';
 
 const ProductsList = () => {
   const { data: products, loading, error } = useFetchProducts();
+  const { theme } = useContext(ThemeContext);
 
   if (loading) return <Spinner />;
 
@@ -13,7 +15,7 @@ const ProductsList = () => {
   }
 
   return (
-    <div className="product-container">
+    <div className="product-container" style={({ backgroundColor: theme.background, color: theme.foreground })}>
       {products.length > 0 &&
         products.map((product) => {
           return <ProductListRow key={product.id} product={product} />;
